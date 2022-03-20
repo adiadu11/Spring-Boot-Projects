03bb009343706224dc29d7c96d651f68e550d704
adiadu11 <adityabalodi.adu@gmail.com>
2022-03-20 16:55:21 +0530

    Handling BadCredentialsException for invalid credential login

--- SuperheroCrud/src/main/java/com/superhero/exception/GlobalExceptionHandler.java
+++ SuperheroCrud/src/main/java/com/superhero/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -51,5 +52,18 @@ public class GlobalExceptionHandler
 	{
 		ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(), new Date(), request.getDescription(false));
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+	
+	/**
+	 * Exception handler for when user logs in with wrong password.
+	 */
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<?> badCredentialsExceptionHandler(BadCredentialsException ex, 
+											   WebRequest request)
+	{
+		ErrorDetails ed = new ErrorDetails(ex.getMessage(), new Date(), 
+				request.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.UNAUTHORIZED);
 	}
 }
